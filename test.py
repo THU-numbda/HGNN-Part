0edd581518df0485a8697c7ff4d3c809d80a1d91
tsb-19 <tsb23@mails.tsinghua.edu.cn>
2025-07-17 22:16:30 +0800

    multiple processes test

--- test.py
+++ test.py
@@ -8,7 +8,7 @@ from utils import *
 from models import GraphPartitionModel
 
 if __name__ == '__main__':
-    torch.cuda.set_device(3)
+    torch.cuda.set_device(0)
     set_seed(42)
     device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     parser = argparse.ArgumentParser()
@@ -46,20 +46,30 @@ if __name__ == '__main__':
     best_cut, best_imbalance = evaluate_partition(initial_partition, hypergraph_vertices, hypergraph_edges, num_partitions)
     reason_time = 0
     vcycle_time = 0
-    for tau in range(10):
+    for tau in range(11):
+        tau_best_cut = float('inf')
         t0 = time.time()
-        partitions = model.sample(data, m=1)
+        partitions = model.sample(data, m=12)
         t1 = time.time()
         reason_time += t1 - t0
         t0 = time.time()
-        cut, imbalance, partition_id = evalPoint(0, partitions[0], hypergraph_vertices, hypergraph_edges, 2, filename, True, True)
-        if cut < best_cut:
-            best_cut = cut
-            best_imbalance = imbalance
-            best_partition_id = partition_id
-            partition_id = partition_id / np.linalg.norm(partition_id) * np.linalg.norm(data.x[:, 4].cpu().numpy())
-            data.x[:, 6] = torch.tensor(partition_id, dtype=torch.float).to(device)
+        processes = []
+        pool = mp.Pool(processes=4)
+        for m in range(len(partitions)):
+            processes.append(pool.apply_async(evalPoint, (m, partitions[m], hypergraph_vertices, hypergraph_edges, num_partitions, filename, True, True)))
+            time.sleep(0.01)
+        pool.close()
+        pool.join()
+        for process in processes:
+            cut, imbalance, partition_id = process.get()
+            tau_best_cut = min(tau_best_cut, cut)
+            if cut < best_cut:
+                best_cut = cut
+                best_imbalance = imbalance
+                best_partition_id = partition_id
         t1 = time.time()
         vcycle_time += t1 - t0
-        print(f'Tau: {tau}, Best Cut: {best_cut}, Imbalance: {best_imbalance:.3f}, Cut: {cut}')
+        print(f'Tau: {tau}, Best Cut: {best_cut}, Imbalance: {best_imbalance:.3f}, Cut: {tau_best_cut}')
+        best_partition_id = best_partition_id / np.linalg.norm(best_partition_id) * np.linalg.norm(data.x[:, 4].cpu().numpy())
+        data.x[:, 6] = torch.tensor(best_partition_id, dtype=torch.float).to(device)
     print(f'Best Cut: {best_cut}, Imbalance: {best_imbalance:.3f}, Preprocess Time: {preprocess_time:.3f}, Reasoning Time: {reason_time:.3f}, V-Cycle Time: {vcycle_time:.3f}')
